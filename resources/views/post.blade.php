297e9a6dd07f104af2ece58e87f57e6f10dc8dbe
FaishalThaariq12 <isalthoriq12@gmail.com>
2022-10-26 22:44:23 +0700

    Memperbaiki eror pada judul  ke-3

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.main')
-
 @section('container')
 <article>
   <h1 class="mb-5">{{ $post->title }}</h1>
 
-  <p>By. M Faishal Thariqulhaq <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+  <p>By. Faishal Thariqulhaq in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+
 
   {!! $post->body !!}
 </article>
