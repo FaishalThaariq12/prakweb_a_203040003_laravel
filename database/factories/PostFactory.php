caa50ba37234f9b558fd28093e4f495b73f3f16d
FaishalThaariq12 <isalthoriq12@gmail.com>
2022-10-27 00:14:35 +0700

    Menyelesaikan Video 10. Factory & Faker

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -2,22 +2,32 @@
 
 namespace Database\Factories;
 
+use App\Models\Post;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
- */
 class PostFactory extends Factory
 {
     /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Post::class;
+
+    /**
      * Define the model's default state.
      *
-     * @return array<string, mixed>
+     * @return array
      */
     public function definition()
     {
         return [
-            //
+            'title' => $this->faker->sentence(mt_rand(2, 8)),
+            'slug' => $this->faker->slug(),
+            'excerpt' => $this->faker->sentence(mt_rand(10, 25)),
+            'body' => $this->faker->paragraph(mt_rand(5, 10)),
+            'user_id' => mt_rand(1, 3),
+            'category_id' => mt_rand(1, 2)
         ];
     }
 }
