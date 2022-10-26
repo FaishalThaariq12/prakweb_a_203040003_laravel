82b1e7a7fa057f313799db0f91f22dede5ee6c3e
FaishalThaariq12 <isalthoriq12@gmail.com>
2022-10-26 23:18:57 +0700

    Menyelesaikan Video 9. Database Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,11 +17,62 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::create([
+            'name' => 'M Faishal Thariqulhaq',
+            'email' => 'isalthoriq12@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::create([
+            'name' => 'Salma',
+            'email' => 'salma@gmail.com',
+            'password' => bcrypt('54321')
+        ]);
+
+        Category::create([
+            'name' => 'Web Programming',
+            'slug' => 'web-programming'
+        ]);
+
+        Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        Post::create([
+            'title' => 'Judul Pertama',
+            'slug' => 'judul-pertama',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi a dignissimos fuga error, voluptatibus temporibus, assumenda nostrum perspiciatis voluptatum atque, voluptates reprehenderit harum eius.',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi a dignissimos fuga error, voluptatibus temporibus, assumenda nostrum perspiciatis voluptatum atque, voluptates reprehenderit harum eius. Qui, placeat! Qui possimus nobis vitae pariatur. Libero, magni, fugiat iusto at suscipit doloremque quis blanditiis dignissimos dolor vero eligendi quas facilis dolores corporis corrupti expedita quasi voluptates accusamus pariatur tempora mollitia exercitationem. Consequuntur quis iusto pariatur, ipsum quia ducimus tempora at, excepturi nostrum asperiores expedita omnis dolore, reiciendis obcaecati cumque? Provident similique adipisci enim excepturi quisquam? Iusto, natus, sunt blanditiis ratione nesciunt ex incidunt, officiis voluptate voluptatibus iure provident adipisci dignissimos? Dolorum nesciunt maxime blanditiis?',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Kedua',
+            'slug' => 'judul-kedua',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi a dignissimos fuga error, voluptatibus temporibus, assumenda nostrum perspiciatis voluptatum atque, voluptates reprehenderit harum eius.',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi a dignissimos fuga error, voluptatibus temporibus, assumenda nostrum perspiciatis voluptatum atque, voluptates reprehenderit harum eius. Qui, placeat! Qui possimus nobis vitae pariatur. Libero, magni, fugiat iusto at suscipit doloremque quis blanditiis dignissimos dolor vero eligendi quas facilis dolores corporis corrupti expedita quasi voluptates accusamus pariatur tempora mollitia exercitationem. Consequuntur quis iusto pariatur, ipsum quia ducimus tempora at, excepturi nostrum asperiores expedita omnis dolore, reiciendis obcaecati cumque? Provident similique adipisci enim excepturi quisquam? Iusto, natus, sunt blanditiis ratione nesciunt ex incidunt, officiis voluptate voluptatibus iure provident adipisci dignissimos? Dolorum nesciunt maxime blanditiis?',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ketiga',
+            'slug' => 'judul-ketiga',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi a dignissimos fuga error, voluptatibus temporibus, assumenda nostrum perspiciatis voluptatum atque, voluptates reprehenderit harum eius.',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi a dignissimos fuga error, voluptatibus temporibus, assumenda nostrum perspiciatis voluptatum atque, voluptates reprehenderit harum eius. Qui, placeat! Qui possimus nobis vitae pariatur. Libero, magni, fugiat iusto at suscipit doloremque quis blanditiis dignissimos dolor vero eligendi quas facilis dolores corporis corrupti expedita quasi voluptates accusamus pariatur tempora mollitia exercitationem. Consequuntur quis iusto pariatur, ipsum quia ducimus tempora at, excepturi nostrum asperiores expedita omnis dolore, reiciendis obcaecati cumque? Provident similique adipisci enim excepturi quisquam? Iusto, natus, sunt blanditiis ratione nesciunt ex incidunt, officiis voluptate voluptatibus iure provident adipisci dignissimos? Dolorum nesciunt maxime blanditiis?',
+            'category_id' => 2,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Keempat',
+            'slug' => 'judul-keempat',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi a dignissimos fuga error, voluptatibus temporibus, assumenda nostrum perspiciatis voluptatum atque, voluptates reprehenderit harum eius.',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi a dignissimos fuga error, voluptatibus temporibus, assumenda nostrum perspiciatis voluptatum atque, voluptates reprehenderit harum eius. Qui, placeat! Qui possimus nobis vitae pariatur. Libero, magni, fugiat iusto at suscipit doloremque quis blanditiis dignissimos dolor vero eligendi quas facilis dolores corporis corrupti expedita quasi voluptates accusamus pariatur tempora mollitia exercitationem. Consequuntur quis iusto pariatur, ipsum quia ducimus tempora at, excepturi nostrum asperiores expedita omnis dolore, reiciendis obcaecati cumque? Provident similique adipisci enim excepturi quisquam? Iusto, natus, sunt blanditiis ratione nesciunt ex incidunt, officiis voluptate voluptatibus iure provident adipisci dignissimos? Dolorum nesciunt maxime blanditiis?',
+            'category_id' => 2,
+            'user_id' => 2
+        ]);
     }
 }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,11 +3,11 @@
 <article>
   <h1 class="mb-5">{{ $post->title }}</h1>
 
-  <p>By. Faishal Thariqulhaq in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+  <p>By. <a href="#" class="text-decoration-none">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
 
 
   {!! $post->body !!}
 </article>
 
-<a href="/blog">Back to Post</a>
+<a href="/blog" class="d-block mt-3">Back to Post</a>
 @endsection
