9c45e128e0d50c78073cce8d8781e5a1fb59289b
FaishalThaariq12 <isalthoriq12@gmail.com>
2022-10-30 12:47:22 +0700

    Menyelesaikan Video 14. View Login & Registration

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
-use App\Models\Post;
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Models\Category;
-use App\Models\User;
+
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -46,10 +48,13 @@ Route::get('/categories', function () {
     ]);
 });
 
+Route::get('/login', [LoginController::class, 'index']);
+Route::get('/register', [RegisterController::class, 'index']);
+
 //Route::get('/categories/{category:slug}', function (Category $category) {
     //return view('posts', [
-    //'title' => "Post by Category : $category->name",
-    //'active' => 'categories',
+     //'title' => "Post by Category : $category->name",
+     //'active' => 'categories',
     //'posts' => $category->posts->load('category', 'author')
     //]);
 ///});
